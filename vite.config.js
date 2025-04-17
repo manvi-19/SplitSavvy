bd7bf2c2d6ca45a3694eb2def4f22408b95df170
manvi-19 <manvitekriwal@gmail.com>
2025-04-17 17:19:51 +0530

    Ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SplitSavvy/', // ✅ correct format
+  base: '/SplitSavvy/',
   plugins: [react()],
 })
